e04a3ddd1d5bcd717b95538e721c24bff4fde00e
DmytroI <dimaiv@me.com>
2018-06-29 15:45:13 +0200

    add routes and links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,34 +1,50 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import { Route, Link } from 'react-router-dom';
+
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost.js';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div className="Blog">
-                <header>
-                    <nav>
-                        <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
-                                pathname: '/new-post',
-                                hash: '#submit',
-                                search: '?quick-submit=true'
-                            }}>New Post</Link></li>
-                        </ul>
-                    </nav>
-                </header>
-                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
-                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink
+                  to="/"
+                  exact
+                  // activeClassName='' generic class name for active route
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={{
+                    pathname: '/new-post',
+                    hash: '#submit',
+                    search: '?quick-submit=true'
+                  }}>
+                  New Post
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <Route path="/" exact component={Posts} />
+        <Switch>
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/:id" exact component={FullPost} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default Blog;
